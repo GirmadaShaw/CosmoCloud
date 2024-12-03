9a47d0c1c4ead7c38471ac84d0ccdaa58402ff69
Paramveer Singh <singhparam.7171@gmail.com>
2024-12-03 09:04:25 +0000

    updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,24 @@ uri = "mongodb+srv://girmada:girmada@girmada.tuopq.mongodb.net/?retryWrites=true
 logging.info("MongoDB URI is set")
 
 
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
 logging.info("Mongo Client set up is complete")
 
 db = client["student_management"] # my database
-logging.info(""""student_management" database set up is complete.  """)
+logging.info(""""student_management" database set up is complete""")
 
 students_collection = db["students"]  # my collection name
-logging.info("""Collection set up by name - "students_collection" is complete  """)
+logging.info("""Collection set up by name - "students_collection" is complete""")
 
+
+try:
+    logging.info("Trying to ping the client...")
+    client.admin.command('ping')
+    logging.info("Successfully connected to MongoDB Atlas client")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+
+except Exception as e:
+    logging.error("Could not connect to the client")
+    print(e)
 
 
